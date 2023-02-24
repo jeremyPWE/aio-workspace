f957e37fbee2fd4b2827d24c23c51a95194b1596
jeremyPWE <jpwijanto@gmail.com>
2023-02-24 16:40:58 +0700

    add head meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,19 @@ import Head from "next/head";
 export default function Home() {
   return (
     <>
-      <Head></Head>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Description of your website or project"
+        />
+        <meta
+          name="keywords"
+          content="List of keywords related to your website or project"
+        />
+        <title>Title of your website or project</title>
+      </Head>
     </>
   );
 }
